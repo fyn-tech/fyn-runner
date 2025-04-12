f3b07575d144a5a53ec12a4f6c6a4c824ab2474a
bevanwsjones <bevanwsjones@gmail.com>
2025-04-12 22:43:42 +0200

    -most data collection completed

--- fyn_runner/system/collection.py
+++ fyn_runner/system/collection.py
@@ -22,62 +22,60 @@ from fyn_runner.server.message import HttpMethod, Message
 
 def collect_system_info(logger, file_manager, server_proxy):
 
-    logger.info("Checking system hardware")
-    hw_file = file_manager.cache_dir / 'hardware_data.json'
+    logger.info("Checking system")
+    hw_file = file_manager.cache_dir / 'system_data.json'
 
-    reader = open(hw_file, 'r', encoding='utf-8')
+    reader = open(hw_file, 'r', encoding='utf-8') if hw_file.exists() else None
     saved_hw_data = json.load(reader) if hw_file.exists() else None
-    reader.close()
-    current_hw_data = _get_hardware_data()
+    if reader:
+        reader.close()
+    current_hw_data = _get_system_data(file_manager, logger)
 
     if current_hw_data != saved_hw_data:
-        logger.info("Hardware has been updated, updating server data.")
-        logger.debug(f"Hardware data:{current_hw_data}")
+        logger.info("System has been updated, updating server data.")
+        logger.debug(f"System data:{current_hw_data}")
 
         server_proxy.push_message(
             Message.json_message(api_path=f"{server_proxy.api_url}:{server_proxy.api_port}/"
-                                 f"hardware_update/{server_proxy.id}", method=HttpMethod.PUT,
+                                 f"update_system/{server_proxy.id}", method=HttpMethod.PUT,
                                  json_data=current_hw_data, header=None, priority=0, params=None)
         )
     else:
-        logger.debug("No change to hardware")
+        logger.debug("No change to system info")
 
     with open(hw_file, 'w', encoding='utf-8') as writer:
         json.dump(current_hw_data, writer, ensure_ascii=False, indent=4)
 
 
-def _get_hardware_data():
-    return {
-        "memory": _get_memory_info(),
-    }
-
-
-def _get_memory_info():
-    ram_info = psutil.virtual_memory()
-    return {
-        "total_gb": ram_info.total
-    }
+def _get_system_data(file_manager, logger):
+    info = _get_os_info()
+    info |= _get_cpu_data()
+    info |= _get_ram_data()
+    info |= _get_disk_data(file_manager, logger)
+    info |= _get_gpu_data()
+    return info
 
 
 def _get_os_info():
     return {
-        'system_name': platform.system_name,
-        'system_release': platform.system_release,
-        'system_version': platform.system_version,
-        'system_architecture': platform.system_architecture,
+        'system_name': platform.system(),
+        'system_release': platform.release(),
+        'system_version': platform.version(),
+        'system_architecture': platform.machine(),
     }
 
 
 def _get_cpu_data():
     cpu_info = get_cpu_info()
     return {
-        'cpu_model': cpu_info.brand_raw,
-        'cpu_clock_speed': cpu_info.hz_advertised_raw,
+        'cpu_model': cpu_info['brand_raw'],
+        'cpu_clock_speed_advertised': cpu_info['hz_advertised'][0],
+        'cpu_clock_speed_actual': cpu_info['hz_actual'][0],
         'cpu_logical_cores': psutil.cpu_count(),
         'cpu_physical_cores': psutil.cpu_count(logical=False),
-        'cpu_cache_l1_size': cpu_info.l1_data_cache_size,
-        'cpu_cache_l2_size': cpu_info.l2_cache_size,
-        'cpu_cache_l3_size': cpu_info.l3_cache_size,
+        'cpu_cache_l1_size': cpu_info['l1_data_cache_size'],
+        'cpu_cache_l2_size': cpu_info['l2_cache_size'],
+        'cpu_cache_l3_size': cpu_info['l3_cache_size'],
     }
 
 
@@ -103,7 +101,9 @@ def _get_disk_data(file_manager, logger):
 
 
 def _get_gpu_data():
-
+    """
+    TODO: This is a bit involved, will do later
+    """
     # 'gpu_vendor': self.gpu_vendor,
     # 'gpu_model': self.gpu_model,
     # 'gpu_memory_size': self.gpu_memory_size,
