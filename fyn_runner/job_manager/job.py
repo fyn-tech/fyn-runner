e29233b019578fda86e9f50fdfa52479de5af886
bevanwsjones <bevanwsjones@gmail.com>
2025-06-11 22:54:40 +0200

    - job now reports final success.

--- fyn_runner/job_manager/job.py
+++ fyn_runner/job_manager/job.py
@@ -18,13 +18,12 @@ from fyn_api_client.models.status_enum import StatusEnum
 from fyn_api_client.models.job_info import JobInfo
 from fyn_api_client.models.patched_job_info_runner_request import PatchedJobInfoRunnerRequest
 from fyn_runner.server.server_proxy import ServerProxy
-from fyn_runner.server.message import Message
-from fyn_runner.job_manager.job_activity_tracking import ActiveJobTracker, ActivityState
+from fyn_runner.job_manager.job_activity_tracking import ActiveJobTracker
 
 
 class Job:  # this is the SimulationMonitor (in its own thread)
     def __init__(self, job: JobInfo, server_proxy: ServerProxy, case_directory: Path,
-                 logger: Logger, activtiy_tracker:ActiveJobTracker):
+                 logger: Logger, activtiy_tracker: ActiveJobTracker):
         self.case_directory: Path = case_directory
         self.job: JobInfo = job
         self.logger: Logger = logger
@@ -37,8 +36,9 @@ class Job:  # this is the SimulationMonitor (in its own thread)
             self._setup()
             self._run()
             self._clean_up()
+            self._update_status(StatusEnum.SD)
         except Exception as e:
-            self.logger.error(f"Job {self.job.id} suffered a runner execption: {e}")
+            self.logger.error(f"Job {self.job.id} suffered a runner exception: {e}")
             self._update_status(StatusEnum.FE)
 
     def _setup(self):
@@ -51,33 +51,32 @@ class Job:  # this is the SimulationMonitor (in its own thread)
 
         # 3. add listeners for commands from server
 
-
     def _run(self):
         # 1. launch job
         self._update_status(StatusEnum.RN)
         # 2. Loop
         # 2. report progress
         # 3. terminate/update if requested (via handlers)
-        
+
     def _clean_up(self):
         self._update_status(StatusEnum.CU)
         # 1. report progress
         # 2. deregister listeners
-        
+
     def _update_status(self, status):
 
-        try:            
+        try:
             self.job.status = status
 
             # Report status to server
             jir = PatchedJobInfoRunnerRequest(status=status)
-            self._job_api.job_manager_runner_partial_update(self.job.id, 
+            self._job_api.job_manager_runner_partial_update(self.job.id,
                                                             patched_job_info_runner_request=jir)
-            
+
             # Update local status
             if self._job_activity_tracker.is_tracked(self.job.id):
                 self._job_activity_tracker.update_job_status(self.job.id, jir.status)
-            else: 
+            else:
                 self._job_activity_tracker.add_job(self.job)
 
             self.logger.debug(f"Job {self.job.id} reported status: {status.value}")
